cef08a41008f628446314201fb236e281d2cd8d0
Your Name <you@example.com>
2021-03-13 19:01:39 +0530

    added jwt

--- controllers/login/login_ctrl.go
+++ controllers/login/login_ctrl.go
@@ -25,6 +25,8 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
+var expirationTime = time.Now().Add(5 * time.Minute)
+
 func Login() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
@@ -36,46 +38,53 @@ func Login() gin.HandlerFunc {
 			c.JSON(resterr.Status, resterr.Message)
 			return
 		}
+
 		var user *users.User
 
 		user, userErr := users.GetUserByUsername(creds.Username)
-		fmt.Println("fetched user", user)
+		//fmt.Println("fetched user", user)
 		if userErr != nil {
-			fmt.Println("unable  to fetch user details in login", userErr)
+			//fmt.Println("unable  to fetch user details in login", userErr)
 			//logger.Info(userErr.Status, nil)
 			c.JSON(400, "login failed")
 			return
 		}
-		fmt.Println()
 		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
 		if err != nil {
 			fmt.Println("password not match")
 			c.JSON(400, "password not match")
 			return
 		}
-
-		expirationTime := time.Now().Add(2 * time.Minute)
-		// Create the JWT claims, which includes the username and expiry time
-		claims := &Claims{
-			Username: creds.Username,
-			StandardClaims: jwt.StandardClaims{
-				// In JWT, the expiry time is expressed as unix milliseconds
-				ExpiresAt: expirationTime.Unix(),
-			},
-		}
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-		// Create the JWT string
-		tokenString, err := token.SignedString(jwtKey)
+		tokenString, err := CreateToken(creds.Username)
 		if err != nil {
-			// If there is an error in creating the JWT return an internal server error
-			c.JSON(500, "error in generating jwt authentication token")
+			c.JSON(500, "error in crating token")
 			return
 		}
-		fmt.Println("jwt ", tokenString)
-		c.SetCookie("token", tokenString, int(claims.ExpiresAt), "/", "localhost", false, true)
-		fmt.Println(c.Writer)
+		c.SetCookie("token", tokenString, int(expirationTime.Unix()), "/", "localhost", false, true)
+		//fmt.Println(c.Writer)
 		c.JSON(200, "login successful")
 	}
+}
+
+func CreateToken(username string) (string, error) {
+
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		//c.JSON(500, "error in generating jwt authentication token")
+		return "", err
+	}
+	return tokenString, nil
 
 }
 
@@ -108,4 +117,44 @@ func ValidateToken(c *gin.Context) bool {
 
 	return true
 
+}
+
+func RefreshToken(c *gin.Context) {
+
+	isValid := ValidateToken(c)
+	if !isValid {
+		fmt.Println("Token is not valid")
+		return
+	}
+	// (END) The code up-till this point is the same as the first part of the `Welcome` route
+	claims := &Claims{}
+	// We ensure that a new token is not issued until enough time has elapsed
+	// In this case, a new token will only be issued if the old token is within
+	// 30 seconds of expiry. Otherwise, return a bad request status
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+		//w.WriteHeader(http.StatusBadRequest)
+		//return "", err
+		fmt.Println("Yet to expire, more than 30 sec")
+		return
+	}
+	// Now, create a new token for the current use, with a renewed expiration time
+	//expirationTime := time.Now().Add(1 * time.Minute)
+	claims.ExpiresAt = expirationTime.Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		//w.WriteHeader(http.StatusInternalServerError)
+		//return "", err
+		fmt.Println("500")
+		return
+	}
+
+	// Set the new token as the users `token` cookie
+	// http.SetCookie(w, &http.Cookie{
+	// 	Name:    "token",
+	// 	Value:   tokenString,
+	// 	Expires: expirationTime,
+	// })
+	//fmt.Println("token refreshed")
+	c.SetCookie("token", tokenString, int(expirationTime.Unix()), "/", "localhost", false, true)
 }
